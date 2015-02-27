75a1533c45fdf90dbd66147dabfb459c72a9403c
ninjapretzel <ninjapretzel@yahoo.com>
2015-02-27 15:35:28 -0700

    Fixes some meta files, adds RaycastHitExtensions

---
+++ Exensions/UnitySpecificExtensions/RaycastHitExtensions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class RaycastHitExtensions {
+	
+	public static Vector3 TrueNormal(this RaycastHit rayhit) {
+		Debug.LogWarning("RaycastHit.TrueNormal does not work properly yet");
+		if (rayhit.collider != null) {
+			MeshCollider mcollider = rayhit.collider as MeshCollider;
+			if (mcollider != null) {
+				Mesh mesh = mcollider.sharedMesh;
+				int triangle = rayhit.triangleIndex * 3;
+				int[] triangles = mesh.triangles;
+				Vector3 v1 = mesh.vertices[triangles[triangle + 0]];
+				Vector3 v2 = mesh.vertices[triangles[triangle + 1]];
+				Vector3 v3 = mesh.vertices[triangles[triangle + 2]];
+				Plane p = new Plane(v2, v1, v3);
+				
+				return p.normal;
+			}
+			
+		}
+		
+		
+		return rayhit.normal;
+	}
+	
+	
+}
