575942adacc33b488d672406844af5c0675934a9
ninjapretzel <ninjapretzel@yahoo.com>
2015-03-27 16:23:59 -0600

    Adds GUIRoot/GUITooltip behaviours.
    GUIRoot automatically draws all open windows
    GUITooltip draws a tooltip at the mouse, based on what object is set as
    the tooltip.

---
+++ Behaviours/GUI/GUIRoot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GUIRoot : MonoBehaviour {
+	
+	public static GUIRoot main;
+	
+	public static List<ZWindow> windows = new List<ZWindow>();
+	
+	public static void AddWindow(ZWindow window) {
+		windows.Add(window);
+	}
+	
+	void Awake() {
+		if (main == null) {
+			main = this;
+			DontDestroyOnLoad(gameObject);
+		} else {
+			Destroy(gameObject);
+		}
+	}
+	
+	void Update() {
+		
+	}
+	
+	void OnGUI() {
+		GUI.depth = 0;
+		
+		foreach (ZWindow window in windows) {
+			window.Draw();
+		}
+		
+		
+		
+		
+	}
+	
+	
+}
---
+++ Behaviours/GUI/GUITooltip.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GUITooltip : MonoBehaviour {
+	
+	public static JsonObject tooltip;
+	
+	public static float tooltip_width = 240;
+	void OnGUI() {
+		GUI.depth = -100;
+		GUI.skin = Resources.Load<GUISkin>("Standard");
+		if (tooltip != null) {
+			DrawTooltip(tooltip);
+			
+			tooltip = null;
+		}
+	}
+	
+	void DrawTooltip(JsonObject tooltip) {
+		float x = tooltip.GetFloat("x");
+		float y = tooltip.GetFloat("y");
+		
+		string content = tooltip.GetString("content");
+		
+		float width = tooltip_width;
+		if (tooltip.ContainsKey("width")) {
+			width = tooltip.GetFloat("width");
+		}
+		float height = GUI.skin.box.CalcHeight(new GUIContent(content), width);
+		
+		if (y + height > Screen.height) { 
+			y = Screen.height - height;
+		}
+		if (x + width > Screen.width) {
+			x = Screen.width - width;
+		}
+		
+		Rect brush = new Rect(x, y, width, height);
+		GUI.Box(brush, content);
+		
+		
+		
+	}
+	
+}
