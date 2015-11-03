b1a84c7695ceb49e3df78a5767b81578a834d83f
ninjapretzel <ninjapretzel@yahoo.com>
2015-11-03 12:20:06 -0700

    Modifies GUIRoot so that it delays the adding and removal of windows to the next Update() to prevent ConcurrentModificationExceptions

--- Behaviours/GUI/GUIRoot.cs
+++ Behaviours/GUI/GUIRoot.cs
@@ -7,21 +7,33 @@ using System.Collections.Generic;
 #endif
 public class GUIRoot : MonoBehaviour {
 	
+	private class WindowInfo { 
+		public string name { get; private set; }
+		public ZWindow window { get; private set; }
+		public int type { get; private set; }
+		public WindowInfo(string n, ZWindow w, int t) {
+			name = n; window = w; type = t;
+		}
+	}
+
 	public static GUIRoot main;
 	
 	public static List<ZWindow> windows = new List<ZWindow>();
-	
+	static List<WindowInfo> delayedAdd = new List<WindowInfo>();
+	static List<string> delayedRemove = new List<string>();
+
 	private static Dictionary<string, ZWindow> binds = new Dictionary<string, ZWindow>();
 	private static Dictionary<string, ZWindow> holdBinds = new Dictionary<string,ZWindow>();
 	private static Dictionary<string, ZWindow> registeredWindows = new Dictionary<string,ZWindow>();
 
-
+	public const int REGISTER = 0;
+	public const int BIND = 1;
+	public const int BINDHOLD = 2;
+		
 	public static void AddWindow(ZWindow window) {
 		windows.Add(window);
 	}
 
-	
-	
 	void Awake() {
 		if (main == null) {
 			main = this;
@@ -32,7 +44,23 @@ public class GUIRoot : MonoBehaviour {
 	}
 	
 	void Update() {
-		
+		while (delayedRemove.Count > 0) {
+			_Remove(delayedRemove[0]);
+			delayedRemove.RemoveAt(0);
+
+		}
+
+		while (delayedAdd.Count > 0) {
+			WindowInfo info = delayedAdd[0];
+			delayedAdd.RemoveAt(0);
+
+			if (info.type == REGISTER) { _Register(info.name, info.window); }
+			if (info.type == BINDHOLD) { _BindHold(info.name, info.window); }
+			if (info.type == BIND) { _BindKey(info.name, info.window); }
+			
+		}
+
+
 		foreach (var pair in binds) {
 			
 			if (ControlStates.Get<bool>(pair.Key)) {
@@ -57,20 +85,24 @@ public class GUIRoot : MonoBehaviour {
 		
 	}
 
-	public static void Register(string key, ZWindow window) {
+	static void _Register(string key, ZWindow window) {
 		registeredWindows[key] = window;
 		if (!windows.Contains(window)) { windows.Add(window); }
 	}
-	
-	public static void BindKey(string key, ZWindow window) {
+
+	static void _BindKey(string key, ZWindow window) {
 		binds[key] = window;
 		if (!windows.Contains(window)) { windows.Add(window); }
 	}
-	
-	public static void BindHold(string key, ZWindow window) {
+	static void _BindHold(string key, ZWindow window) {
 		holdBinds[key] = window;
 		if (!windows.Contains(window)) { windows.Add(window); }
 	}
+
+	public static void Register(string key, ZWindow window) { delayedAdd.Add(new WindowInfo(key, window, REGISTER)); }
+	public static void BindKey(string key, ZWindow window) { delayedAdd.Add(new WindowInfo(key, window, BIND)); }
+	public static void BindHold(string key, ZWindow window) { delayedAdd.Add(new WindowInfo(key, window, BINDHOLD)); }
+	
 	public static bool HasWindow(string key) {
 		return registeredWindows.ContainsKey(key)
 			|| binds.ContainsKey(key)
@@ -88,7 +120,12 @@ public class GUIRoot : MonoBehaviour {
 		}
 	}
 
-	public static ZWindow Remove(string key) {
+
+	public static void Remove(string key) {
+		delayedRemove.Add(key);
+	}
+
+	static void _Remove(string key) {
 		ZWindow window = null;
 		if (binds.ContainsKey(key)) {
 			window = binds[key];
@@ -105,7 +142,6 @@ public class GUIRoot : MonoBehaviour {
 			windows.Remove(window);
 			registeredWindows.Remove(key);
 		}
-		return window;
 	}
 	
 	public static void HideAllWindows() {
