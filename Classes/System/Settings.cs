929bb724eb2d33ce40cecc04e7ba332263db7256
ninjapretzel <ninjapretzel@yahoo.com>
2015-11-08 16:59:39 -0700

    Adds some more settings options

--- Classes/System/Settings.cs
+++ Classes/System/Settings.cs
@@ -8,8 +8,15 @@ public partial class Settings {
 
 	public static Settings instance = new Settings();
 
+	public int qualityLevel = 3;
+	
 	public float musicVolume = 1;
 	public float soundVolume = 1;
 	public float overscanRatio = 0;
+
+	public string userName = "New User";
+	public Color color = Color.red;
+	public float sensitivity = 5;
+	
 
 }
