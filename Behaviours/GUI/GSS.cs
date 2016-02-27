71c6e2e423328880abe08aabc7acb24a973cb9d6
ninjapretzel <ninjapretzel@yahoo.com>
2016-02-27 12:34:07 -0700

    Adds sick styling system - Stylesheets can be applied in a file 'Resources/styles.json' 'Resources/defaultStyles.json' is used to store global defaults, and these default styles can be overriden with content from styles.json
    As of this commit, UI components supported are:
    Image
    Text
    Button
    ScrollView/Scrollbar
    
    Objects are found by name, and styles are applied based off of cascade rules. ex.
    {
        "nameSuffix":"Panel",
        "tagWith":"panel",
    }
    will find objects ending in 'Panel' and apply the 'panel' style to them

---
+++ Behaviours/GUI/GSS.cs
@@ -0,0 +1,234 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
+
+
+public partial class GSS : MonoBehaviour {
+	public string tagClass = "";
+	string curTag = "";
+
+	public string styleClass = "";
+	string curStyle = "";
+
+}
+
+#if XtoJSON
+
+
+public partial class GSS : MonoBehaviour {
+
+	static JsonObject styles = LoadStyles();
+	[NonSerialized] private static int lastDefaultHash = 0;
+	[NonSerialized] private static int lastStyleHash = 0;
+
+	static JsonObject LoadStyles() {
+		string defaultStyleJson = Resources.Load<TextAsset>("defaultStyles").text;
+		lastDefaultHash = defaultStyleJson.GetHashCode();
+
+		JsonObject obj = Json.Parse(defaultStyleJson) as JsonObject;
+		TextAsset styleFile = Resources.Load<TextAsset>("styles");
+		if (styleFile != null) {
+			string styleJson = styleFile.text;
+			lastStyleHash = styleJson.GetHashCode();
+
+			JsonObject loadedStyles = Json.Parse(styleJson) as JsonObject;
+
+			obj.SetRecursively(loadedStyles);
+		}
+
+		//Debug.Log("Loaded Stylesheets:"+obj.PrettyPrint());
+		return obj;
+	}
+
+	public static bool ReloadStyles() {
+		string defaultStyleJson = Resources.Load<TextAsset>("defaultStyles").text;
+		bool reload = defaultStyleJson.GetHashCode() != lastDefaultHash;
+
+		TextAsset styleFile = Resources.Load<TextAsset>("styles");
+		if (styleFile != null) {
+			string styleJson = styleFile.text;
+			reload |= styleJson.GetHashCode() != lastStyleHash;
+
+		}
+
+		if (reload) { styles = LoadStyles(); }
+		return reload;
+	}
+
+	static float scaleRatio { get { return ((float)Screen.height) / 720f; } }
+	
+	public static JsonArray cascades { get { return styles["cascades"] as JsonArray; } }
+
+	void Start() {
+		if (styleClass != curStyle || tagClass != curTag) {
+			//Debug.Log("Updating style on " + name);
+			UpdateStyle(tagClass, styleClass);
+			curStyle = styleClass;
+			curTag = tagClass;
+		}
+		
+	}
+	
+	void Update() {
+		if (styleClass != curStyle || tagClass != curTag) {
+			//Debug.Log("Updating style on " + name);
+			UpdateStyle(tagClass, styleClass);
+			curStyle = styleClass;
+			curTag = tagClass;
+		}
+
+		
+	}
+
+
+	void UpdateStyle(string tag, string style) { ApplyStyle(this, tag, style); }
+	void ApplyStyle(JsonObject style) { ApplyStyle(this, style); }
+
+	public static void ApplyStyle(Component c, string tag, string style) {
+		if (styles.ContainsKey(tag) || styles.ContainsKey(style)) {
+			JsonObject styleObj = styles.Get<JsonObject>(style);
+			JsonObject tagObj = styles.Get<JsonObject>(tag);
+
+			JsonObject combObj = new JsonObject();
+			if (tagObj != null) { combObj.Set(tagObj); }
+			if (styleObj != null && tag != style) { combObj.Set(styleObj); }
+
+			//Debug.Log("Built Style [" + tag + "." + style + "] :" + combObj.PrettyPrint());
+			ApplyStyle(c, combObj);
+		}
+
+	}
+	public static void ApplyStyle(Component c, JsonObject style) {
+		string inherit = style.Get<string>("inherit");
+		if (inherit != null && inherit != "" && styles.ContainsKey(inherit)) {
+			JsonObject inheritedStyle = styles.Get<JsonObject>(inherit);
+			ApplyStyle(c, inheritedStyle);
+		}
+
+		Image img = c.GetComponent<Image>();
+		Button btn = c.GetComponent<Button>();
+		Text txt = c.GetComponent<Text>();
+		ScrollRect scr = c.GetComponent<ScrollRect>();
+
+		if (img != null) { ApplyImageStyle(img, style); }
+		if (btn != null) { ApplyButtonStyle(btn, style); }
+		if (txt != null) { ApplyTextStyle(txt, style); }
+		if (scr != null) { ApplyScrollRectStyle(scr, style); }
+
+		foreach (var pair in style) {
+			if (pair.Value.isObject) {
+				Transform target = c.transform.Find(pair.Key);
+				if (target != null) { 
+					GSS gss = target.GetComponent<GSS>();
+					if (gss != null) { Destroy(gss); }
+					ApplyStyle(target, pair.Value as JsonObject); 
+				}
+			}
+		}
+
+	}
+
+	static void ApplyImageStyle(Image img, JsonObject style) {
+		if (style.ContainsKey("image")) { img.sprite = Resources.Load<Sprite>(style.Get<string>("image")); }
+
+
+		if (style.ContainsKey("color")) { img.color = GetColor(style, "color"); }
+		if (style.ContainsKey("material")) { img.material = Resources.Load<Material>(style.Get<string>("material")); }
+		if (style.ContainsKey("raycastTarget")) { img.raycastTarget = style.Get<bool>("raycastTarget"); }
+		if (style.ContainsKey("imageType")) { img.type = style.Get<Image.Type>("imageType"); }
+		if (style.ContainsKey("fillCenter")) { img.fillCenter = style.Get<bool>("fillCenter"); }
+		if (style.ContainsKey("preserveAspect")) { img.preserveAspect = style.Get<bool>("preserveAspect"); }
+		//TBD: Figure out fillOrigin and what the numbers mean
+		//if (style.ContainsKey("fillMethod")) { img.fillMethod = style.Get<Image.FillMethod>("fillMethod"); }
+		//if (style.ContainsKey("fillOrigin")) { img.fillOrigin = style.Get<Image.
+
+		
+	}
+
+
+
+	static void ApplyButtonStyle(Button btn, JsonObject style) {
+		ApplySelectableStyle(btn, style);
+	}
+
+	static void ApplyScrollbarStyle(Scrollbar scb, JsonObject style) {
+		ApplySelectableStyle(scb, style);
+	}
+
+	static void ApplyTextStyle(Text txt, JsonObject style) {
+		if (style.ContainsKey("font")) { txt.font = Resources.Load<Font>(style.Get<string>("font")); }
+		if (style.ContainsKey("fontStyle")) { txt.fontStyle = style.Get<FontStyle>("fontStyle"); }
+
+		bool scale = style.Extract("sizeScale", true);
+		if (style.ContainsKey("fontSize")) { txt.fontSize = (int)(style.Get<float>("fontSize") * (scale ? scaleRatio : 1)); }
+		if (style.ContainsKey("richText")) { txt.supportRichText = style.Get<bool>("richText"); }
+		if (style.ContainsKey("alignment")) { txt.alignment = style.Get<TextAnchor>("alignment"); }
+		if (style.ContainsKey("alignByGeometry")) { txt.alignByGeometry = style.Get<bool>("alignByGeometry"); }
+		if (style.ContainsKey("horizontalOverflow")) { txt.horizontalOverflow = style.Get<HorizontalWrapMode>("horizontalOverflow"); }
+		if (style.ContainsKey("verticalOverflow")) { txt.verticalOverflow = style.Get<VerticalWrapMode>("verticalOverflow"); }
+
+		if (style.ContainsKey("bestFit")) { txt.resizeTextForBestFit = style.Get<bool>("bestFit"); }
+		if (style.ContainsKey("color")) { txt.color = GetColor(style, "color"); }
+		if (style.ContainsKey("material")) { txt.material = Resources.Load<Material>(style.Get<string>("material")); }
+
+		if (style.ContainsKey("raycastTarget")) { txt.raycastTarget = style.Get<bool>("raycastTarget"); }
+
+	}
+
+	static void ApplyScrollRectStyle(ScrollRect scr, JsonObject style) {
+		if (style.ContainsKey("horizontal")) {
+			bool setHorizontal = style.Get<bool>("horizontal");
+			scr.horizontal = setHorizontal;
+			scr.horizontalScrollbarVisibility = setHorizontal ? ScrollRect.ScrollbarVisibility.AutoHideAndExpandViewport : ScrollRect.ScrollbarVisibility.AutoHide;
+			scr.horizontalScrollbarSpacing = 0;
+		}
+		if (style.ContainsKey("vertical")) {
+			bool setVertical = style.Get<bool>("vertical");
+			scr.vertical = setVertical;
+			scr.verticalScrollbarVisibility = setVertical ? ScrollRect.ScrollbarVisibility.AutoHideAndExpandViewport : ScrollRect.ScrollbarVisibility.AutoHide;
+			scr.verticalScrollbarSpacing = 0;
+		}
+
+		if (style.ContainsKey("inertia")) { scr.inertia = style.Get<bool>("inertia"); }
+		if (style.ContainsKey("decelerationRate")) { scr.decelerationRate = style.Get<float>("decelerationRate"); }
+		if (style.ContainsKey("scrollSensitivity")) { scr.scrollSensitivity = style.Get<float>("scrollSensitivity"); }
+
+	}
+
+
+
+
+
+	static void ApplySelectableStyle(Selectable sct, JsonObject style) {
+		if (style.ContainsKey("interactable")) { sct.interactable = style.Get<bool>("interactable"); }
+		if (style.ContainsKey("transition")) { sct.transition = style.Get<Selectable.Transition>("transition"); }
+		
+		sct.spriteState = ApplySpriteStateStyle(sct.spriteState, style);
+		sct.colors = ApplyColorBlockStyle(sct.colors, style);
+
+	}
+	static ColorBlock ApplyColorBlockStyle(ColorBlock block, JsonObject style) {
+		if (style.ContainsKey("normalColor")) { block.normalColor = GetColor(style, "normalColor"); }
+		if (style.ContainsKey("highlightedColor")) { block.highlightedColor = GetColor(style, "highlightedColor"); }
+		if (style.ContainsKey("pressedColor")) { block.pressedColor = GetColor(style, "pressedColor"); }
+		if (style.ContainsKey("disabledColor")) { block.disabledColor = GetColor(style, "disabledColor"); }
+		if (style.ContainsKey("colorMultiplier")) { block.colorMultiplier = style.Get<float>("colorMultiplier"); }
+		if (style.ContainsKey("fadeDuration")) { block.fadeDuration = style.Get<float>("fadeDuration"); }
+		return block;
+	}
+
+	static SpriteState ApplySpriteStateStyle(SpriteState sprites, JsonObject style) {
+		if (style.ContainsKey("highlightedSprite")) { sprites.highlightedSprite = Resources.Load<Sprite>(style.Get<string>("highlightedSprite")); }
+		if (style.ContainsKey("pressedSprite")) { sprites.pressedSprite = Resources.Load<Sprite>(style.Get<string>("pressedSprite")); }
+		if (style.ContainsKey("disabledSprite")) { sprites.disabledSprite = Resources.Load<Sprite>(style.Get<string>("disabledSprite")); }
+
+		return sprites;
+	}
+
+	static Color GetColor(JsonObject o, string k) { return Colors.ParseHex(o.Get<string>(k)); }
+}
+
+
+#endif
---
+++ Behaviours/GUI/StyleChildrenWithGSS.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[ExecuteInEditMode]
+public class StyleChildrenWithGSS : MonoBehaviour {
+
+
+	[NonSerialized] private int lastWidth = -1;
+	[NonSerialized] private int lastHeight = -1;
+
+	void Awake() {
+		ProcessAllChildren(true);
+		
+
+	}
+
+	void Update() {
+		
+		if (GSS.ReloadStyles() || lastWidth != Screen.width || lastHeight != Screen.height) {
+			ProcessAllChildren(true);
+			lastHeight = Screen.height;
+			lastWidth = Screen.width;
+
+		}
+
+#if !UNITY_EDITOR
+		if (Application.isPlaying) { 
+			Destroy(this);	
+		}
+#endif
+
+	}
+
+	void ProcessAllChildren(bool includeInactvie = false) {
+		Transform[] children = transform.GetComponentsInChildren<Transform>(includeInactvie);
+
+		for (int i = children.Length-1; i >= 0; i--) {
+			var child = children[i];
+			foreach (var style in GSS.cascades) {
+				Process(style as JsonObject, child);
+			}
+		}
+
+	}
+
+	public void Process(JsonObject style, Component c) {
+		string name = c.gameObject.name;
+		string namePrefix = style.Get<string>("namePrefix");
+		string nameSuffix = style.Get<string>("nameSuffix");
+		string nameContains = style.Get<string>("nameContains");
+		string tagWith = style.Get<string>("tagWith");
+		string styleWith = style.Get<string>("styleWith");
+
+		if ((namePrefix != null && namePrefix != "" && name.StartsWith(namePrefix))
+			|| (nameContains != null && nameContains != "" && name.Contains(nameContains))
+			|| (nameSuffix != null && nameSuffix != "" && name.EndsWith(nameSuffix))) {
+
+			if (c.GetComponent<GSS>() == null) {
+				GSS.ApplyStyle(c, tagWith, styleWith);
+
+				//GSS g = c.AddComponent<GSS>();
+				//g.tagClass = tagWith;
+				//g.styleClass = styleWith;
+			}
+		}
+	}
+	
+
+}
