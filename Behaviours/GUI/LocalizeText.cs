218b7367aa26fc7a96ade38ae3a941a9a208916b
ninjapretzel <ninjapretzel@yahoo.com>
2016-05-20 18:52:45 -0600

    adds text-mesh-pro support, and makes 'missing' markup an optional addition.

--- Behaviours/GUI/LocalizeText.cs
+++ Behaviours/GUI/LocalizeText.cs
@@ -3,20 +3,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using System;
+#if TMPRO
+using TMPro;
+#endif
 
 public class LocalizeText : MonoBehaviour {
 
 	Language lastLang;
 	Text uiText;
-	string originalString;
+#if TMPRO
+	TextMeshPro proText;
+	TextMeshProUGUI uiProText;
+#endif
 
+	string originalString;
+	public bool noMarkup = false;
+	
 
 	void Start() {
 		uiText = GetComponent<Text>();
+#if TMPRO
+		proText = GetComponent<TextMeshPro>();
+		uiProText = GetComponent<TextMeshProUGUI>();
 
+		if (proText != null) {
+			originalString = proText.text;
+			if (noMarkup) { proText.text = Localization.Localize_NOMARKUP(originalString); }
+			else { proText.text = Localization.Localize(originalString); }
+			
+			lastLang = Localization.language;
+		} else if (uiProText != null) {
+			originalString = uiProText.text;
+			if (noMarkup) { uiProText.text = Localization.Localize_NOMARKUP(originalString); }
+			else { uiProText.text = Localization.Localize(originalString); }
+
+			lastLang = Localization.language;
+		} else //LOL conditional compiled else if chain... 
+#endif	
 		if (uiText != null) {
 			originalString = uiText.text;
-			uiText.text = Localization.Localize(originalString);
+			if (noMarkup) { uiText.text = Localization.Localize_NOMARKUP(originalString); }
+			else { uiText.text = Localization.Localize(originalString); }
 			lastLang = Localization.language;
 		}
 		
@@ -24,7 +51,21 @@ public class LocalizeText : MonoBehaviour {
 	
 	void Update() {
 		if (Localization.language != lastLang) {
-			uiText.text = Localization.Localize(originalString);
+#if TMPRO
+			if (proText != null) {
+				if (noMarkup) { proText.text = Localization.Localize_NOMARKUP(originalString); }
+				else { proText.text = Localization.Localize(originalString); }
+			} else if (uiProText != null) {
+				if (noMarkup) { uiProText.text = Localization.Localize_NOMARKUP(originalString); }
+				else { uiProText.text = Localization.Localize(originalString); }
+			} else //goddamnit why must I do this?
+#endif
+			if (uiText != null) {
+				if (noMarkup) { uiText.text = Localization.Localize_NOMARKUP(originalString); }
+				else { uiText.text = Localization.Localize(originalString); }
+			}
+
+
 			lastLang = Localization.language;
 		}
 	}
