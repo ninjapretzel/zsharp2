8226954f0402d7f5f9973211379d90007876b233
wfowler1 <will.fowler4201@gmail.com>
2016-05-26 14:00:48 -0600

    DefaultButton is now DefaultSelection.

--- Behaviours/GUI/DefaultSelection.cs
+++ Behaviours/GUI/DefaultSelection.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-[RequireComponent(typeof(Button))]
-public class DefaultButton : MonoBehaviour {
+[RequireComponent(typeof(Selectable))]
+public class DefaultSelection : MonoBehaviour {
 	protected virtual void OnEnable() {
 		EventSystem.current.SetSelectedGameObject(gameObject);
 	}
