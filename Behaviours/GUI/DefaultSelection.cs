a65348223318389d020a173a937e4ba222c14816
ninjapretzel <ninjapretzel@yahoo.com>
2016-08-05 15:21:47 -0600

    Fixes problems with DefaultSelections not being highlighted.

--- Behaviours/GUI/DefaultSelection.cs
+++ Behaviours/GUI/DefaultSelection.cs
@@ -4,11 +4,18 @@ using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Selectable))]
 public class DefaultSelection : MonoBehaviour {
-	protected virtual void OnEnable() {
-		if (EventSystem.current != null) {
+	private bool _inited = false;
+
+	protected virtual void Update() {
+		if (!_inited && EventSystem.current != null) {
 			if (EventSystem.current.currentSelectedGameObject == null || EventSystem.current.currentSelectedGameObject.activeInHierarchy == false) {
 				EventSystem.current.SetSelectedGameObject(gameObject);
 			}
 		}
+		_inited = true;
+	}
+
+	protected void OnDisable() {
+		_inited = false;
 	}
 }
