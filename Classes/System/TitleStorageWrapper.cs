9dbb6d5683c3b0489152f3e2ded4306d47a10a6f
ninjapretzel <ninjapretzel@yahoo.com>
2016-10-07 13:15:43 -0600

    Adds TitleStorageWrapper class to wrap the TitleStorage class

---
+++ Classes/System/TitleStorageWrapper.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+#if UNITY_XBOXONE
+using DataPlatform;
+using System.Text;
+using Storage;
+#endif
+
+
+public static class TitleStorageWrapper {
+
+#if UNITY_XBOXONE && !UNITY_EDITOR
+	public static TitleStorage store = null;
+
+	/// <summary>
+	/// 2^18 = 256k of memory.
+	/// Should be more than enough for any level data we need to save.
+	/// </summary>
+	public const int MAX_DATA_SIZE = 2 << 18;
+
+	public static void Load(string filename, Action<bool, byte[], int> callback) {
+		byte[] data = new byte[MAX_DATA_SIZE];
+		if (store != null) {
+			store.DownloadFileAsync(filename, data, (storage, op, size) => {
+				callback(op.Success, op.Buffer, (int)op.Size);
+			});
+		}
+	}
+		
+
+	
+
+	public static void Save(string filename, string data) { Save(filename, GZip.Compress(data)); }
+	public static void Save(string filename, byte[] data) {
+		if (store != null) {
+			store.UploadFileAsync(filename, data, (storage, op, size) => {
+
+			});
+		}
+	}
+#endif
+
+	
+
+
+}
