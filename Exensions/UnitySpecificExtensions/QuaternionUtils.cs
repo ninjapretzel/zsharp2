c8cafe7b18be5d17b22151681d8ec1a35b91006c
ninjapretzel <ninjapretzel@yahoo.com>
2016-10-07 13:14:33 -0600

    Adds some documentation

--- Exensions/UnitySpecificExtensions/QuaternionUtils.cs
+++ Exensions/UnitySpecificExtensions/QuaternionUtils.cs
@@ -9,18 +9,28 @@ public static class QuaternionUtils {
 	//Get the rotation that would be multiplied by to rotate a to b
 	public static Quaternion To(this Quaternion a, Quaternion b) { return Quaternion.Inverse(a) * b; }
 	
+	
+	/// <summary> Returns a copy of the quaternion with the eulerAngles Z component set to zero</summary>
+	/// <param name="q">Source rotation</param>
+	/// <returns>Rotation built from source rotation with the eulerAngles Z component set to zero</returns>
 	public static Quaternion FlattenZ(this Quaternion q) {
 		Vector3 e = q.eulerAngles;
 		e.x = 0;
 		return Quaternion.Euler(e);
 	}
 	
+	/// <summary> Returns a copy of the quaternion with the eulerAngles Y component set to zero</summary>
+	/// <param name="q">Source rotation</param>
+	/// <returns>Rotation built from source rotation with the eulerAngles Y component set to zero</returns>
 	public static Quaternion FlattenY(this Quaternion q) {
 		Vector3 e = q.eulerAngles;
 		e.x = 0;
 		return Quaternion.Euler(e);
 	}
-	
+
+	/// <summary> Returns a copy of the quaternion with the eulerAngles X component set to zero</summary>
+	/// <param name="q">Source rotation</param>
+	/// <returns>Rotation built from source rotation with the eulerAngles X component set to zero</returns>
 	public static Quaternion FlattenX(this Quaternion q) {
 		Vector3 e = q.eulerAngles;
 		e.z = 0;
